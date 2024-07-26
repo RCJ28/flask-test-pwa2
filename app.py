2b1e82293a7da18b8ad3578ad9ea510f2b62f77c
Jaya Chandra <140376728+RCJ28@users.noreply.github.com>
2024-07-26 21:38:37 +0530

    commit changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, send_file, request, jsonify
 
 app = Flask(__name__)
 
@@ -13,6 +13,17 @@ def serve_sw():
 @app.route('/manifest.json')
 def serve_manifest():
     return send_file('manifest.json', mimetype='application/manifest+json')
+
+@app.route('/add', methods=['POST'])
+def add():
+    try:
+        num1 = float(request.form['num1'])
+        num2 = float(request.form['num2'])
+        result = num1 + num2
+    except ValueError:
+        result = "Invalid input. Please enter numbers only."
+    
+    return jsonify(result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
